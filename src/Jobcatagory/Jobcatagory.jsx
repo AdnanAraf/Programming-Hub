a1e7a47a4029792239e15e29515eb2ff9e48582e
AdnanAraf <arafadnan10@gmail.com>
2023-04-10 03:38:20 +0600

    second commit

--- src/Jobcatagory/Jobcatagory.jsx
+++ src/Jobcatagory/Jobcatagory.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Jobcatagory = () => {
-  return <div></div>;
+  const [catagory, setcatagory] = useState([]);
+
+  useEffect(() => {
+    fetch("JobCatagoryfakedata.json")
+      .then((res) => res.json())
+      .then((data) => setcatagory(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="text-center ">
+        <h1 className="font-semibold mt-[130px] text-[48px]">
+          Job Category List
+        </h1>
+        <div>
+          <p className="font-thin">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className="grid grid-cols-4 my-[32px] mx-[100px] cursor-pointer">
+          {catagory.map((item) => (
+            <div className="h-[220px] w-[243px] bg-purple-50 rounded-lg">
+              <img
+                className="h-[80px] w-[80px] mt-[40px] mx-[40px] rounded-lg"
+                src={item.img}
+              ></img>
+              <h1 className="mx-[40px] font-bold mt-[10px]">{item.title}</h1>
+              <p className="mx-[40px] mt-[10px] text-gray-400">
+                {item.subtitle}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Jobcatagory;
